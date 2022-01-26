40ad41a58fc30409232dfc0246058806b47c862e
Nkwe Justice Mapoulo <nkwejustice1@gmail.com>
2022-01-26 22:32:07 +0200

    first commit

--- src/main/java/com/example/demo/ShopEazyCloudConfigApplication.java
+++ src/main/java/com/example/demo/ShopEazyCloudConfigApplication.java
@@ -3,10 +3,12 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 
 @EnableConfigServer
 @SpringBootApplication
+@EnableEurekaClient
 public class ShopEazyCloudConfigApplication {
 
 	public static void main(String[] args) {
